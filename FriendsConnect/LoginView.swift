68a95ceab1e99a96defb608a8a8eb33df83d7d92
6110450014 <Kritsada.lakhasont@gmail.com>
2023-09-24 21:18:00 +1000

    Trying to fix bug on Login page
    Idk how the padding moving base on the text I put in.

--- FriendsConnect/LoginView.swift
+++ FriendsConnect/LoginView.swift
@@ -13,61 +13,110 @@ struct LoginView: View {
     @State private var password = ""
     
     var body: some View {
-        VStack {
-            Spacer()
-            ZStack{
-                RoundedRectangle(cornerRadius: 10) // Set the corner radius
-                    .frame(width: 250.0, height: 250.0)
-                    .foregroundColor(Color.purple)
-                    .border(Color.black, width: 2)
-                Text("Friend(s) Connect")
-                    .font(.title)
-                    .fontWeight(.light)
-            }
-            .padding(.all, 15.0)
-            TextField("Username", text: $username).textFieldStyle(PlainTextFieldStyle()) // This removes the default border
-                .padding() // Add padding for a better visual appearance
-                .border(Color.white, width: 2) // Add a custom border
-                .padding()
-            SecureField("Password", text: $password)
-                .textFieldStyle(PlainTextFieldStyle())
-                .padding()
-                .border(Color.white, width: 2)
-                .padding()
-            Button(action: {
-                    // Handle login button action here
-            }) {
-                Text("Login")
-                    .padding()
-                    .background(Color.purple)
-                    .foregroundColor(.white)
-                    .cornerRadius(5.0)
-            }.padding(.vertical, 19.0)
-            HStack{
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 250.0, height: 250.0)
+                        .foregroundColor(Color.purple)
+                        .border(Color.black, width: 2)
+                    
+                    Text("Friend(s) Connect")
+                        .font(.title)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                }
+                .padding(.all, 15.0)
+                
+                // Custom TextField with white placeholder text
+                CustomTextField(placeholder: "Username", text: $username)
+                CustomTextField(placeholder: "Password", text: $password, isSecure: true)
+                
                 Button(action: {
                     // Handle login button action here
                 }) {
-                    Text("Register")
+                    Text("Login")
                         .padding()
+                        .background(Color.purple)
                         .foregroundColor(.white)
                         .cornerRadius(5.0)
                 }
-                Button(action: {
-                    // Handle login button action here
-                }) {
-                    Text("forget password?")
-                        .padding()
-                        .foregroundColor(.white)
-                        .cornerRadius(5.0)
+                .padding(.vertical, 19.0)
+                .accessibilityLabel("Login")
+                
+                HStack {
+                    Button(action: {
+                        // Handle registration button action here
+                    }) {
+                        Text("Register")
+                            .padding()
+                            .foregroundColor(Color.purple)
+                            .cornerRadius(5.0)
+                    }
+                    
+                    Button(action: {
+                        // Handle "Forget Password?" button action here
+                    }) {
+                        Text("Forget Password?")
+                            .padding()
+                            .foregroundColor(Color.purple)
+                            .cornerRadius(5.0)
+                    }
                 }
+                
+                Spacer()
             }
-            Spacer()
-        }.ignoresSafeArea()
+        }
     }
 }
 
-struct Login_Previews: PreviewProvider {
+struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
 }
+
+// CustomTextField with bold and white placeholder text and static underline
+struct CustomTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    var isSecure: Bool = false
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(.white) // Set placeholder text color
+                        .font(.body) // Apply bold font weight
+                        .padding(.horizontal, 8)
+                }
+                if isSecure {
+                    SecureField("", text: $text)
+                        .textFieldStyle(PlainTextFieldStyle()) // Apply plain style
+                        .padding(10) // Add padding to the SecureField for a better look
+                        .background(Color.clear) // Clear background color
+                        .alignmentGuide(.top) { d in d[.top] } // Maintain static position
+                } else {
+                    TextField("", text: $text)
+                        .textFieldStyle(PlainTextFieldStyle()) // Apply plain style
+                        .padding(10) // Add padding to the TextField for a better look
+                        .background(Color.clear) // Clear background color
+                        .alignmentGuide(.top) { d in d[.top] } // Maintain static position
+                }
+            }
+            .foregroundColor(.white)
+            .accentColor(.white)
+            
+            Rectangle() // Add a static underline
+                .frame(height: 1) // Adjust the height of the underline
+                .foregroundColor(.white) // Set the underline color
+        }
+        .padding(.horizontal, 8) // Adjust horizontal padding
+    }
+}
+
