62de1b34945722fe2858557b5158fe32fb981302
6110450014 <Kritsada.lakhasont@gmail.com>
2023-10-06 00:50:08 +1100

    add button action in register

--- FriendsConnect/Register.swift
+++ FriendsConnect/Register.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct Register: View {
+    @State private var userData = UserData(username: "", password: "", uid: "", name: "")
+
     @State private var username = ""
     @State private var password = ""
     @State private var uid = ""
     @State private var name = ""
+    
     
     var body: some View {
         ZStack {
@@ -33,7 +36,17 @@ struct Register: View {
                 CustomTextField(placeholder: "Password", text: $password, isSecure: true)
                     .padding([.top, .leading, .bottom], 10)
                 Button(action: {
-                    // Handle login button action here
+                    // Store the user's data in userData
+                    userData.username = username
+                    userData.password = password
+                    userData.uid = uid
+                    userData.name = name
+                    
+                    // Optionally, you can print the stored data for testing purposes
+                    print("User Data: \(userData)")
+                    
+                    // Display a message to indicate that data is stored
+                    showAlert = true
                 }) {
                     Text("Create")
                         .padding()
@@ -43,11 +56,33 @@ struct Register: View {
                 }
                 .padding(.vertical, 19.0)
                 .accessibilityLabel("Create")
+
+                // Add a state variable to control the alert
+                @State private var showAlert = false
+
+                // Use an alert to display a message
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Data Stored"),
+                        message: Text("User data has been stored successfully."),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
+
+
+                .padding(.vertical, 19.0)
+                .accessibilityLabel("Create")
                 
                 Spacer()
             }
         }
     }
+}
+struct UserData {
+    var username: String
+    var password: String
+    var uid: String
+    var name: String
 }
 
 struct Register_Previews: PreviewProvider {
